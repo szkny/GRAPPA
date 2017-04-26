6685a7abd4a61a963836223426c8668ed1b3e403
szkny <suzuki.s1008@gmail.com>
2017-04-27 01:23:27 +0900

    initial source

---
+++ include/define.h
@@ -0,0 +1,22 @@
+#ifndef Define_h_
+#define Define_h_
+
+/***************************/
+
+#ifndef Randf_
+#define Randf_
+//0~1の一様乱数の作成する関数
+double randf(void){
+    rand();rand();rand();rand();rand();rand();
+    return rand()*(1.0/RAND_MAX);
+}
+#endif
+
+
+#ifndef PI
+#define PI 3.14159265359
+#endif
+
+/***************************/
+
+#endif //"Define_h_"
